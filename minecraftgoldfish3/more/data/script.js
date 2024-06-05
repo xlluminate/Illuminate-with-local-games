a9fad94d3a53f62926fb46ceb107ff187b9f5737
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-06-05 13:34:11 -0500

    Update script.js

--- minecraftgoldfish3/more/data/script.js
+++ minecraftgoldfish3/more/data/script.js
@@ -27,13 +27,14 @@ function extractURL() {
     return null;
 }
 const save = () => {
-    var cookieData = document.cookie.split(';').map(function (c) {
-        var i = c.indexOf('=');
-        return [c.substring(0, i), c.substring(i + 1)];
-    });
-    var saveCookieData = JSON.stringify(JSON.stringify(cookieData));
+    //var cookieData = document.cookie.split(';').map(function (c) {
+        //var i = c.indexOf('=');
+        //return [c.substring(0, i), c.substring(i + 1)];
+    //});
+    //var saveCookieData = JSON.stringify(JSON.stringify(cookieData));
     const link = document.createElement("a");
-    var content = saveCookieData;
+    //var content = saveCookieData;
+    var content = document.cookie;
     const file = new Blob([content], { type: 'text/plain' });
     link.href = URL.createObjectURL(file);
     link.download = "data.txt";
@@ -46,13 +47,16 @@ function load() {
     if (newCookieData == null || newCookieData == '') {
         window.close();
     }
-     if (confirm('Are you sure you want to replace your site data with this value?') == true) {
+    if (confirm('Are you sure you want to replace your site data with this value?') == true) {
+        document.cookie = newCookieData;
         //var newCookieData = extractURL();
         //loadData(newCookieData);
-         var cookieData = JSON.parse(newCookieData);
-         JSON.parse(newCookieData).forEach(function (arr) {
-             document.cookie = arr[0] + '=' + arr[1];
-         });
+         //var cookieData = JSON.parse(newCookieData);
+         //console.log(newCookieData);
+         //console.log(JSON.parse(newCookieData));
+         //JSON.parse(newCookieData).forEach(function (arr) {
+             //document.cookie = arr[0] + '=' + arr[1];
+         //});
          confirm('Success, your data has been replaced.');
          window.close();
     } else {
